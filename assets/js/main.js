94995380f74ddae20475b6b0d24cef067a86b192
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-08-13 11:36:20 -0300

    feito o prompt de escolha de personagens

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+function start () {
+
+    while(true) {
+        var personagemEscolhido = prompt('Escolha seu personagen: \n 1 - Pequenalf \n 2 - Sam e Frodo \n 3 - Misterimir')
+
+        if (personagemEscolhido == 1) {
+
+            return location = ('../terra-media/assets/fases/pequenalf-fase1.html');
+        } else {
+            alert('Opção invalida, escolha denovo')
+        }
+    }
+
+}
