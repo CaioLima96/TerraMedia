5bb00d6349b698caabcb92473b9c46d0312d0c04
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-08-14 01:47:46 -0300

    mudado cor do <a> do footer do index

--- assets/js/pequenalf/pequenalf-fase3.js
+++ assets/js/pequenalf/pequenalf-fase3.js
@@ -1,27 +1,6 @@
-// function escolhaPequenalfFase3 () {
-
-//     let avancarPequenalfFase3 = document.getElementById("main-container").innerHTML = `
-            
-//     <h1>Fase 3 - Resurrección</h1>
-
-//     <section id="content">
-
-//         <img src="../../img/personagens/pequenalf-o-branquinho-banner.jpg" alt="Pequenalf, O Branquinho">
-
-//         <p>Pequenalf, O Branquinho</p>
-
-//     </section>
-
-//     <button onclick="parte2PequenalfFase3()"><p>Próxima Fase</p></button>
-//     `;
-
-//     return avancarPequenalfFase3;
-// }
-
-
-//função que me manda pra parte 2 do pequenalf fase 3
 function parte2PequenalfFase3() {
 
+   
 
     let despedidaPequenalfFase3 =  document.getElementById("main-container").innerHTML = `
             
@@ -35,10 +14,15 @@ function parte2PequenalfFase3() {
 
     </section>
 
-    <audio autoplay> <source src="../../music/epic-sax-guy.mp3" type="audio/mpeg"></audio>
+    <audio autoplay id="myAudio"> <source src="../../music/epic-sax-guy.mp3" type="audio/mpeg"></audio>
 
     <a href="../../../index.html#personagens" id="gameOverButton"><p>Jogar de novo</p></a>
     `;
-
+    
     return despedidaPequenalfFase3;
+}
+
+function myAudioVolumeDown () {
+    var audio = document.getElementById("myaudio");
+    audio.volume = 0.2;
 }
